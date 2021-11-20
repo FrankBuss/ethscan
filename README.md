6ff0ca3537567f5dd1c3b03d61ab482e377e1a4e
Frank Buss <fb@frank-buss.de>
2021-11-20 07:42:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ethscan
-Scans the Ethereum network for USDT ERC-20 token transfer transactions
+## Scans the Ethereum network for USDT ERC-20 token transfer transactions
+
+This is a Rust command line program for scanning the Ethereum blockchain for USDT transfers within a time span and amount span. To run it, first install Rust, e.g. from https://rustup.rs.
+
+Then you can build it with `cargo build --release`. Then you can run it like this:
+
+```
+./target/release/ethscan http://localhost:8545 2021-11-20 2021-11-21 100000 1000000000
+
+search filter:
+
+date from: 2021-11-20 00:00:00 UTC
+date to: 2021-11-21 00:00:00 UTC
+amount from: 100000 USDT
+amount to: 1000000000 USDT
+
+time (UTC),amount (USDT),transaction
+2021-11-20 06:33:05,482440.00,https://etherscan.io/tx/0xfe69e999c6844a46a6722a090ee3a9cfe6338c2d02e721840f280953d84c5270
+2021-11-20 06:33:05,600887.31,https://etherscan.io/tx/0x8b175f228c3e38d2cc28f4ab6b4bf1a6c6156883045e7c986ec47c0fe5f63157
+2021-11-20 06:33:05,221350.48,https://etherscan.io/tx/0xc360f4e183576848701193f7a456234a0e4bb849a94c7ea960b740da9088d830
+2021-11-20 06:32:34,518533.00,https://etherscan.io/tx/0xfc4475e64b1f20b90d7837ea8b4129e1ed0b0fe9a4f0240338f59eca4b125572
+2021-11-20 06:31:30,109976.00,https://etherscan.io/tx/0xba5ff95164e88ccfb6aaf9101ada7750ec51f935e14a421db5793f90753510b7
+2021-11-20 06:30:29,214000.00,https://etherscan.io/tx/0x965dba12cd3c08f391e7b6a64d96d9b93ee13944339709995362b377b864fe30
+2021-11-20 06:30:09,161111.00,https://etherscan.io/tx/0x849d668938a5306f1c202cb9c8fc342767856d777a8ce4058cf0b67b264b7375
+2021-11-20 06:30:02,134655.46,https://etherscan.io/tx/0xd4d528bba884f1c3f89a8bd2ffb7b5dc37b47814bff7c51bed8cad79b306606b
+2021-11-20 06:29:20,2650000.00,https://etherscan.io/tx/0xfff84cdd25b480089e601649b93d3b8ca97f4e5e21b7e0d0572a8448a022c499
+2021-11-20 06:28:36,100000.00,https://etherscan.io/tx/0x66d52c1fdfb99253143a20b652136ecd758b3282265dbca100180e83f96103be
+...
+```
+
+It needs a fully synced local installation of the geth node, or you can use a service like https://quiknode.pro as well for the RPC interface, but this can be very slow and expensive, because it has to make potentially millions of API requests, depending on the filter period.
+
+TODO: currently it scans all blocks backwards, starting from the latest block. This could be optimized with binary searching for the start block, if the time span to test is long ago. And it could be enhanced to allow other evaluations as well, for example eth transactions, or other tokens.
---
+++ src/main.rs
@@ -0,0 +1,183 @@
+use chrono::prelude::*;
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime};
+use std::{env, process};
+use web3::transports::Http;
+use web3::types::*;
+use web3::Web3;
+
+#[derive(Debug, Copy, Clone)]
+struct Filter {
+    pub date_from: i64,
+    pub date_to: i64,
+    pub amount_from: f64,
+    pub amount_to: f64,
+    pub contract: H160,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Transfer {
+  pub amount: f64,
+  pub tx_hash: H256,
+}
+
+fn timestamp_to_utc(timestamp: i64) -> String {
+    let naive = NaiveDateTime::from_timestamp(timestamp, 0);
+    let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+    datetime.format("%Y-%m-%d %H:%M:%S").to_string()
+}
+
+fn date_to_timestamp(date: &String) -> i64 {
+    let date = NaiveDate::parse_from_str(date, "%Y-%m-%d").unwrap();
+    date.and_time(NaiveTime::from_hms(0, 0, 0)).timestamp()
+}
+
+fn data_to_usdt(data: &Bytes) -> f64 {
+    (U256::from_big_endian(&data.0[16..]).as_u128() as f64) / 1000000.0
+}
+
+/// test if a transaction matches the filter, and then return the transfer information
+async fn test_transaction(
+    web3: &Web3<Http>,
+    tx: &Transaction,
+    filter: &Filter,
+) -> web3::Result<Vec<Transfer>> {
+    let mut result = Vec::new();
+    if let Some(to) = tx.to {
+        if to != filter.contract {
+            return Ok(result);
+        }
+    } else {
+        return Ok(result);
+    }
+    let receipt = web3.eth().transaction_receipt(tx.hash).await?.unwrap();
+    for log in receipt.logs {
+        let topics = log.topics;
+        if log.address != filter.contract {
+            continue;
+        }
+        if topics.len() != 3 {
+            continue;
+        }
+        //let from = H160::from_slice(&topics[1].0[12..]);
+        //let to = H160::from_slice(&topics[2].0[12..]);
+        let amount = data_to_usdt(&log.data);
+        if amount < filter.amount_from || amount > filter.amount_to {
+            continue;
+        }
+        let transfer = Transfer {
+          amount: amount,
+          tx_hash: tx.hash,
+        };
+        result.push(transfer);
+    }
+    Ok(result)
+}
+
+/**
+Test all transactions of a block, or optionally only the specified transaction, and print the transfer information. Return the number of transactions and amount sum.
+*/
+async fn test_block(
+    web3: &Web3<Http>,
+    block: &Block<Transaction>,
+    tx_hash: Option<H256>,
+    filter: &Filter,
+) -> web3::Result<(u64, f64)> {
+    let time = block.timestamp.as_u64() as i64;
+    let time_string = timestamp_to_utc(time);
+    let mut sum: f64 = 0.0;
+    let mut count: u64 = 0;
+    for tx in &block.transactions {
+        if let Some(tx_hash) = tx_hash {
+            if tx_hash != tx.hash {
+                continue;
+            }
+        }
+        let transfers = test_transaction(web3, tx, filter).await?;
+        for transfer in transfers {
+            println!(
+                "{},{:.2},https://etherscan.io/tx/0x{:x}",
+                time_string, transfer.amount, tx.hash, 
+            );
+            count += 1;
+            sum += transfer.amount;
+        }
+    }
+    Ok((count, sum))
+}
+
+#[tokio::main]
+async fn main() -> web3::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 6 {
+        eprintln!(
+            "usage: {} RPC-URL date-from date-to amount-from amount-to",
+            args[0]
+        );
+        eprintln!(
+            "example: {} http://localhost:8545 2021-11-01 2021-11-20 1000 1100",
+            args[0]
+        );
+        process::exit(1);
+    }
+
+    let url = &args[1];
+    let usdt_contract = "dac17f958d2ee523a2206206994597c13d831ec7"
+        .parse::<H160>()
+        .unwrap();
+    let filter = Filter {
+        date_from: date_to_timestamp(&args[2]),
+        date_to: date_to_timestamp(&args[3]),
+        amount_from: args[4].parse::<f64>().unwrap(),
+        amount_to: args[5].parse::<f64>().unwrap(),
+        contract: usdt_contract,
+    };
+
+    println!("search filter:");
+    println!("");
+    println!("date from: {} UTC", timestamp_to_utc(filter.date_from));
+    println!("date to: {} UTC", timestamp_to_utc(filter.date_to));
+    println!("amount from: {:2} USDT", filter.amount_from);
+    println!("amount to: {:2} USDT", filter.amount_to);
+    println!("");
+
+    // open RPC connection
+    let transport = web3::transports::Http::new(url)?;
+    let web3 = web3::Web3::new(transport);
+
+    // search all blocks, starting from the latest
+    let mut block_id = BlockId::Number(BlockNumber::Latest);
+    println!("time (UTC),amount (USDT),transaction");
+    let mut count_all: u64 = 0;
+    let mut sum_all: f64 = 0.0;
+    loop {
+        let block = web3.eth().block_with_txs(block_id).await?.unwrap();
+
+        // stop at genesis block
+        let id = block.number.unwrap().as_u64();
+        if id == 1 {
+            break;
+        }
+
+        // previous block
+        block_id = BlockId::Number((id - 1).into());
+
+        let time = block.timestamp.as_u64() as i64;
+        if time < filter.date_from || time > filter.date_to {
+            continue;
+        }
+        if time < filter.date_from {
+            break;
+        }
+        let (count, sum) = test_block(&web3, &block, None, &filter).await?;
+        count_all += count;
+        sum_all += sum;
+    }
+    println!("");
+    println!(
+        "number of transactions: {}, amount sum: {:.2} USDT",
+        count_all, sum_all
+    );
+    println!("");
+
+    Ok(())
+}
